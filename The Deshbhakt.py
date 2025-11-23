766b76fc7483a05f588296a444776269b13f1f3e
Yash <158742388+Yash888-Dev@users.noreply.github.com>
2025-11-23 22:36:20 +0530

    Update The Deshbhakt.py

--- The Deshbhakt.py
+++ The Deshbhakt.py
@@ -17,10 +17,6 @@ with col1:
     likes = st.number_input("Likes", min_value=0, help="Enter total number of likes")
     comments = st.number_input("Comments", min_value=0, help="Total number of comments")
     views = st.number_input("Views", min_value=0, help="Number of views in the video")
-    likes_per_view = st.number_input("Likes per View", min_value=0.0, step=0.01,
-                                    help="Ratio of likes to views (e.g., 0.05)")
-    comments_per_view = st.number_input("Comments per View", min_value=0.0, step=0.01,
-                                        help="Ratio of comments to views")
 
 
 with col2:
@@ -30,12 +26,12 @@ with col2:
 
 
 input_data = pd.DataFrame([[likes, comments, views,
-                            likes_per_view, comments_per_view,
                             is_short, publish_day, publish_month]],
-                            columns=["likes","comments","views","likes_per_view","comments_per_view","is_short","publish_day","publish_month"])
+                            columns=["likes","comments","views","is_short","publish_day","publish_month"])
 
 prediction = model.predict(input_data)[0]
 if prediction == 1:
     st.success("Prediction: **Popular** 🎉", icon="✅")
 else:
+
     st.info("Prediction: **Not Popular** 😐", icon="ℹ️")
